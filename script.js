d213361db71c115b87402aceaceeb32a08e2f394
Vinixz007 <148581003+Vinixz007@users.noreply.github.com>
2025-06-11 10:33:41 -0300

    as

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ window.addEventListener('load', revealOnScroll);
 
 // Carrossel
 const images = [
-  './img/campo.png',
-  './img/malte.png',
-  './img/cidade.png'
+  './img/cafeee.jpg',
+  './img/exportação.webp',
+  './img/fazendas.jpg'
 ];
 let currentIndex = 0;
 
